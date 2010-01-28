7326c49165fadf2d66da4deddf639a1734a85026
julien <julien@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-01-28 12:57:14 +0000

    Group common code into management module and add a XrdpException class
    git-svn-id: file:///home/code/svn/xrdp/trunk@31 c7ce0e6c-3862-4c07-88d1-63b9e066504f

--- python-xrdp/User.py
+++ python-xrdp/User.py
@@ -19,65 +19,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import os
-import socket
-import struct
 from xml.dom import minidom
 
+from Management import _ManagementProcessRequest
+
 USER_PREF_UNKNOW		= "UNKNOW"
-
-
-def _UserGetSocket():
-	filename = "/tmp/management"
-	
-	if not os.path.exists(filename):
-		return None
-	
-	# ToDo: check is rw access on the file
-	
-	return filename
-
-
-def _UserProcessRequest(request):
-	path = _UserGetSocket()
-	if path is None:
-		return None
-	
-	s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-	try:
-		s.connect(path)
-	except socket.error,err:
-		print "Unable to connect: ",err
-		return None	
-	
-	data = request.toxml()
-		
-	s.send(struct.pack('>I', len(data)))
-	s.send(data)
-	
-	buffer = s.recv(4)
-	try:
-		packet_len = struct.unpack('>I', buffer)[0]
-	except Exception, e:
-		print "packet recv syntax error"
-		s.close()
-		return None
-	
-	data = s.recv(packet_len)
-	s.close()
-	
-	try:
-		document = minidom.parseString(data)
-	except:
-		print "server didn't return XML"
-		return None
-	
-	if document.documentElement.nodeName == "error":
-		print "server return error"
-		return None
-	
-
-	return document
 
 
 def _UserFormatRequest(username_, type_, action_, key_, value_ = None ):
@@ -97,7 +43,7 @@ def _UserFormatRequest(username_, type_, action_, key_, value_ = None ):
 
 
 def UserGetUserPref(username, key):
-	doc = _UserProcessRequest(_UserFormatRequest(username, "user_conf", "get", key ))
+	doc = _ManagementProcessRequest(_UserFormatRequest(username, "user_conf", "get", key ))
 	
 	if doc is None:
 		return USER_PREF_UNKNOW
@@ -112,7 +58,7 @@ def UserGetUserPref(username, key):
 
 
 def UserSetUserPref(username, key, value):
-	doc = _UserProcessRequest(_UserFormatRequest(username, "user_conf", "set", key, value ))
+	doc = _ManagementProcessRequest(_UserFormatRequest(username, "user_conf", "set", key, value ))
 	
 	if doc is None:
 		return False
--- python-xrdp/__init__.py
+++ python-xrdp/__init__.py
@@ -18,5 +18,15 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+from Exception import *
 from Session import *
+from User import *
 from Vchannel import *
+
+def isRunning():
+	try:
+		SessionGetList()
+	except:
+		return False
+	
+	return True
