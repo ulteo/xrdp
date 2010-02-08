5d99ef5ea77d987046fd4202f54fbf499fecadbf
samuel <samuel@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-02-08 14:49:08 +0000

    add Debian packaging support
    git-svn-id: file:///home/code/svn/xrdp/trunk@62 c7ce0e6c-3862-4c07-88d1-63b9e066504f

---
+++ autogen.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# check needed tools
+tools=( aclocal libtoolize autoheader autoconf automake libtool )
+for tool in "${tools[@]}"; do
+  which $tool > /dev/null
+  if ! test $? -eq 0
+  then
+    echo "error, install $tool"
+    exit 1
+  fi
+done
+
+# build configure.in using svn revno
+if [ -d .svn ]; then
+    revision=$(LC_ALL=C svn info . | awk '/^Revision: / {printf "%05d\n", $2}')
+    sed -e "s/@REVISION@/${revision}/g" \
+        < "configure.ac.in" > "configure.in"
+fi
+
+# ????
+CONFIG_PATH=$PWD/sesman/tools/config.c
+if [ ! -e $CONFIG_PATH ]; then
+  echo "# create sesman/tools/config.c link #"
+  ln -s ../config.c $CONFIG_PATH
+fi
+
+# create configure file
+echo "# launch aclocal #"
+aclocal
+
+echo "# launch libtoolize #"
+libtoolize --force
+
+echo "# launch autoconf #"
+autoconf --force
+
+echo "# launch autoheader #"
+autoheader --force
+
+echo "# launch automake #"
+automake --add-missing --copy --force-missing
