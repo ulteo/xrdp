3077a4b4f9beea64c8beb335c3b1b214b28558f7
samuel <samuel@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-02-19 10:13:24 +0000

    fix packaging issue
    git-svn-id: file:///home/code/svn/xrdp/trunk@154 c7ce0e6c-3862-4c07-88d1-63b9e066504f

--- autogen.sh
+++ autogen.sh
@@ -13,10 +13,12 @@ done
 
 # build configure.in using svn revno
 if [ -d .svn ]; then
-    revision=$(LC_ALL=C svn info . | awk '/^Revision: / {printf "%05d\n", $2}')
-    sed -e "s/@REVISION@/${revision}/g" < configure.ac.in > configure.in
-    sed -e "s/@REVISION@/${revision}/g" < setup.py.in     > setup.py
+  revision=$(LC_ALL=C svn info . | awk '/^Revision: / {printf "%05d\n", $2}')
+else
+  revision=0
 fi
+sed -e "s/@REVISION@/${revision}/g" < configure.ac.in > configure.in
+sed -e "s/@REVISION@/${revision}/g" < setup.py.in     > setup.py
 
 # ????
 CONFIG_PATH=$PWD/sesman/tools/config.c
