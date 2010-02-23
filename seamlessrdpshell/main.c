245245d18731ee4d210e612a19b6faba3b539d75
david <david@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-02-23 12:53:06 +0000

    Bugfix: suppress carry return in seticon message
    git-svn-id: file:///home/code/svn/xrdp/trunk@179 c7ce0e6c-3862-4c07-88d1-63b9e066504f

--- seamlessrdpshell/main.c
+++ seamlessrdpshell/main.c
@@ -483,7 +483,6 @@ get_icon(Window win_in, Window win_out )
 
 			if(message_length >1000 || i == width * height -1)
 			{
-				sprintf(buffer_pos,"\n");
 				send_message(buffer, strlen(buffer));
 
 				message_id++;
