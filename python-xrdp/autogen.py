49f0c3e0a0ad54f8b306fccc90a20a9180619479
samuel <samuel@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-03-08 16:03:30 +0000

    autogen script is now written in Python
    git-svn-id: file:///home/code/svn/xrdp/trunk@190 c7ce0e6c-3862-4c07-88d1-63b9e066504f

---
+++ python-xrdp/autogen.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+import os
+import pysvn
+
+# Detect the version
+if os.environ.has_key("OVD_VERSION"):
+    version = os.environ["OVD_VERSION"]
+
+else:
+    c = pysvn.Client()
+    revision = c.info(".")["revision"].number
+    version = "99.99~trunk+svn%05d"%(revision)
+
+f = file("setup.py.in", "r")
+content = f.read()
+f.close()
+
+content = content.replace("@VERSION@", str(version))
+
+f = file("setup.py", "w")
+f.write(content)
+f.close()
