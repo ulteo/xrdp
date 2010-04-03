2f75113620ea2f84a2ab08d924d383ea5a67434f
david <david@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-04-03 20:18:21 +0000

    Dev: add constant declaration to rdpdr channel
    git-svn-id: file:///home/code/svn/xrdp/trunk@231 c7ce0e6c-3862-4c07-88d1-63b9e066504f

--- sesman/chansrv/rdpdr/rdpdr.h
+++ sesman/chansrv/rdpdr/rdpdr.h
@@ -123,6 +123,104 @@
 #define IRP_MJ_DIRECTORY_CONTROL                0x0000000C
 #define IRP_MJ_LOCK_CONTROL                     0x00000011
 
+/* InformationClass */
+#define FileFsVolumeInformation                 0x00000001
+#define FileFsSizeInformation                   0x00000003
+#define FileFsAttributeInformation              0x00000005
+#define FileFsFullSizeInformation               0x00000007
+#define FileFsDeviceInformation                 0x00000004
+
+/* DesiredAccess */
+/*    for File */
+#define FILE_READ_DATA                          0x00000001
+#define FILE_WRITE_DATA                         0x00000002
+#define FILE_APPEND_DATA                        0x00000004
+#define FILE_READ_EA                            0x00000008
+#define FILE_WRITE_EA                           0x00000010
+#define FILE_EXECUTE                            0x00000020
+#define FILE_READ_ATTRIBUTES                    0x00000080
+#define FILE_WRITE_ATTRIBUTES                   0x00000100
+#define DELETE                                  0x00010000
+#define READ_CONTROL                            0x00020000
+#define WRITE_DAC                               0x00040000
+#define WRITE_OWNER                             0x00080000
+#define SYNCHRONIZE                             0x00100000
+#define ACCESS_SYSTEM_SECURITY                  0x01000000
+#define MAXIMUM_ALLOWED                         0x02000000
+#define GENERIC_ALL                             0x10000000
+#define GENERIC_EXECUTE                         0x20000000
+#define GENERIC_WRITE                           0x40000000
+#define GENERIC_READ                            0x80000000
+
+/*    for Directory */
+#define FILE_LIST_DIRECTORY                     0x00000001
+#define FILE_ADD_FILE                           0x00000002
+#define FILE_ADD_SUBDIRECTORY                   0x00000004
+#define FILE_READ_EA                            0x00000008
+#define FILE_WRITE_EA                           0x00000010
+#define FILE_TRAVERSE                           0x00000020
+#define FILE_DELETE_CHILD                       0x00000040
+#define FILE_READ_ATTRIBUTES                    0x00000080
+#define FILE_WRITE_ATTRIBUTES                   0x00000100
+#define DELETE                                  0x00010000
+#define READ_CONTROL                            0x00020000
+#define WRITE_DAC                               0x00040000
+#define WRITE_OWNER                             0x00080000
+#define SYNCHRONIZE                             0x00100000
+#define ACCESS_SYSTEM_SECURITY                  0x01000000
+#define MAXIMUM_ALLOWED                         0x02000000
+#define GENERIC_ALL                             0x10000000
+#define GENERIC_EXECUTE                         0x20000000
+#define GENERIC_WRITE                           0x40000000
+#define GENERIC_READ                            0x80000000
+
+/* FileAttributes */
+#define FILE_ATTRIBUTE_ARCHIVE                  0x00000020
+#define FILE_ATTRIBUTE_COMPRESSED               0x00000800
+#define FILE_ATTRIBUTE_DIRECTORY                0x00000010
+#define FILE_ATTRIBUTE_ENCRYPTED                0x00004000
+#define FILE_ATTRIBUTE_HIDDEN                   0x00000002
+#define FILE_ATTRIBUTE_NORMAL                   0x00000080
+#define FILE_ATTRIBUTE_NOT_CONTENT_INDEXED      0x00002000
+#define FILE_ATTRIBUTE_OFFLINE                  0x00001000
+#define FILE_ATTRIBUTE_READONLY                 0x00000001
+#define FILE_ATTRIBUTE_REPARSE_POINT            0x00000400
+#define FILE_ATTRIBUTE_SPARSE_FILE              0x00000200
+#define FILE_ATTRIBUTE_SYSTEM                   0x00000004
+#define FILE_ATTRIBUTE_TEMPORARY                0x00000100
+
+/* sharedAccess */
+#define FILE_SHARE_READ                         0x00000001
+#define FILE_SHARE_WRITE                        0x00000002
+#define FILE_SHARE_DELETE                       0x00000004
+
+/* CreateDisposition */
+#define FILE_SUPERSEDE                          0x00000000
+#define FILE_OPEN                               0x00000001
+#define FILE_CREATE                             0x00000002
+#define FILE_OPEN_IF                            0x00000003
+#define FILE_OVERWRITE                          0x00000004
+#define FILE_OVERWRITE_IF                       0x00000005
+
+/* CreateOptions */
+#define FILE_DIRECTORY_FILE                     0x00000001
+#define FILE_WRITE_THROUGH                      0x00000002
+#define FILE_SEQUENTIAL_ONLY                    0x00000004
+#define FILE_NO_INTERMEDIATE_BUFFERING          0x00000008
+#define FILE_SYNCHRONOUS_IO_ALERT               0x00000010
+#define FILE_SYNCHRONOUS_IO_NONALERT            0x00000020
+#define FILE_NON_DIRECTORY_FILE                 0x00000040
+#define FILE_COMPLETE_IF_OPLOCKED               0x00000100
+#define FILE_NO_EA_KNOWLEDGE                    0x00000200
+#define FILE_RANDOM_ACCESS                      0x00000800
+#define FILE_DELETE_ON_CLOSE                    0x00001000
+#define FILE_OPEN_BY_FILE_ID                    0x00002000
+#define FILE_OPEN_FOR_BACKUP_INTENT             0x00004000
+#define FILE_NO_COMPRESSION                     0x00008000
+#define FILE_RESERVE_OPFILTER                   0x00100000
+#define FILE_OPEN_REPARSE_POINT                 0x00200000
+#define FILE_OPEN_NO_RECALL                     0x00400000
+#define FILE_OPEN_FOR_FREE_SPACE_QUERY          0x00800000
 
 
 struct device
