9d0f1fbc00cd78d477692f8c2da993740c5799ec
david <david@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-10-07 13:23:30 +0000

    Code cleaning: the code for the session was not very lisible
    git-svn-id: file:///home/code/svn/xrdp/trunk@428 c7ce0e6c-3862-4c07-88d1-63b9e066504f

--- sesman/scp_v0.c
+++ sesman/scp_v0.c
@@ -53,67 +53,75 @@ scp_v0_process(struct SCP_CONNECTION* c, struct SCP_SESSION* s)
   tc_mutex_lock(session_creation_lock);
   data = auth_userpass(s->username, s->password);
 
-  if (data)
+  if (data == 0)
   {
-    s_item = session_get_bydata(s->username);
-    if (s_item != 0)
-    {
-      display = s_item->display;
-      auth_end(data);
-      if (s_item->status == SESMAN_SESSION_STATUS_TO_DESTROY)
-      {
-      	log_message(&(g_cfg->log), LOG_LEVEL_WARNING, "Session for user %s is in destroy, unable to initialize a new session");
-      	scp_v0s_deny_connection(c, "Your last session is currently \nended, retry later");
-      }
-      else
-      {
-      	session_update_status_by_user(s->username, SESMAN_SESSION_STATUS_ACTIVE);
-      	log_message(&(g_cfg->log), LOG_LEVEL_INFO, "switch from status DISCONNECTED to ACTIVE");
-      }
-    }
-    else
-    {
-      LOG_DBG(&(g_cfg->log), "pre auth");
-      if (1 == access_login_allowed(s->username))
-      {
-        log_message(&(g_cfg->log), LOG_LEVEL_INFO, "granted TS access to user %s", s->username);
-        if (SCP_SESSION_TYPE_XVNC == s->type)
-        {
-          log_message(&(g_cfg->log), LOG_LEVEL_INFO, "starting Xvnc session...");
-          display = session_start(s->width, s->height, s->bpp, s->username,
-                                  s->password, data, SESMAN_SESSION_TYPE_XVNC,
-                                  s->domain, s->program, s->directory, s->keylayout);
-        }
-        else
-        {
-          log_message(&(g_cfg->log), LOG_LEVEL_INFO, "starting X11rdp session...");
-          display = session_start(s->width, s->height, s->bpp, s->username,
-                                  s->password, data, SESMAN_SESSION_TYPE_XRDP,
-                                  s->domain, s->program, s->directory, s->keylayout);
-        }
-      }
-      else
-      {
-        display = 0;
-      }
-    }
-    if (display == 0)
-    {
-      auth_end(data);
-      data = 0;
-      log_message(&(g_cfg->log), LOG_LEVEL_WARNING, "User %s are not allow to start session", s->username);
-      scp_v0s_deny_connection(c, "Unable to launch the session\nPlease contact\nyour administrator\n");
-    }
-    else
-    {
-      scp_v0s_allow_connection(c, display);
-    }
+  	log_message(&(g_cfg->log), LOG_LEVEL_WARNING, "User %s failed to authenticate", s->username);
+  	scp_v0s_deny_connection(c, "Your username or \nyour password is invalid");
+    tc_mutex_unlock(session_creation_lock);
+    return;
+  }
+
+  s_item = session_get_bydata(s->username);
+  if (s_item != 0)
+  {
+  	log_message(&(g_cfg->log), LOG_LEVEL_INFO, "A session for User %s already exist", s->username);
+  	display = s_item->display;
+  	if (s_item->status == SESMAN_SESSION_STATUS_TO_DESTROY)
+  	{
+  		log_message(&(g_cfg->log), LOG_LEVEL_WARNING, "Session for user %s is in destroy, unable to initialize a new session");
+  		scp_v0s_deny_connection(c, "Your last session is currently \nended, retry later");
+  	}
+  	else
+  	{
+  		session_update_status_by_user(s->username, SESMAN_SESSION_STATUS_ACTIVE);
+  		log_message(&(g_cfg->log), LOG_LEVEL_INFO, "switch from status DISCONNECTED to ACTIVE");
+    	scp_v0s_allow_connection(c, display);
+  	}
+
+    auth_end(data);
+    tc_mutex_unlock(session_creation_lock);
+    return;
+  }
+
+  log_message(&(g_cfg->log), LOG_LEVEL_DEBUG, "No session already started for the user %s", s->username);
+  if (access_login_allowed(s->username) == 0)
+  {
+  	log_message(&(g_cfg->log), LOG_LEVEL_WARNING, "User %s is not allow to start session", s->username);
+  	display = 0;
+  	scp_v0s_deny_connection(c, "You are not allowed\nto start a session\n");
+
+  	auth_end(data);
+    tc_mutex_unlock(session_creation_lock);
+    return;
+  }
+
+  log_message(&(g_cfg->log), LOG_LEVEL_INFO, "granted TS access to user %s", s->username);
+  if (SCP_SESSION_TYPE_XVNC == s->type)
+  {
+  	log_message(&(g_cfg->log), LOG_LEVEL_INFO, "starting Xvnc session for the user %s ...", s->username);
+  	display = session_start(s->width, s->height, s->bpp, s->username,
+  			s->password, data, SESMAN_SESSION_TYPE_XVNC,
+  			s->domain, s->program, s->directory, s->keylayout);
   }
   else
   {
-    log_message(&(g_cfg->log), LOG_LEVEL_WARNING, "User %s failed to authenticate", s->username);
-  	scp_v0s_deny_connection(c, "Your username or \nyour password is invalid");
+  	log_message(&(g_cfg->log), LOG_LEVEL_INFO, "starting X11rdp session for the user %s ...", s->username);
+  	display = session_start(s->width, s->height, s->bpp, s->username,
+  			s->password, data, SESMAN_SESSION_TYPE_XRDP,
+  			s->domain, s->program, s->directory, s->keylayout);
   }
+
+  auth_end(data);
+  if (display == 0)
+  {
+  	data = 0;
+  	scp_v0s_deny_connection(c, "Unable to launch the session\nPlease contact\nyour administrator\n");
+  }
+  else
+  {
+  	scp_v0s_allow_connection(c, display);
+  }
+
   tc_mutex_unlock(session_creation_lock);
 }
 
