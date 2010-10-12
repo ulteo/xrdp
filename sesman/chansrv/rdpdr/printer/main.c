8ac7f91b648593e746bd9fa5982773fd6b6ffd38
david <david@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-10-12 17:08:55 +0000

    Typo: loglevel switch
    git-svn-id: file:///home/code/svn/xrdp/trunk@476 c7ce0e6c-3862-4c07-88d1-63b9e066504f

--- sesman/chansrv/rdpdr/printer/main.c
+++ sesman/chansrv/rdpdr/printer/main.c
@@ -338,7 +338,7 @@ printer_devicelist_announce(struct stream* s)
     	printer_device_list_reply(handle, STATUS_SUCCESS);
     	continue;
     }
-    log_message(l_config, LOG_LEVEL_DEBUG, "rdpdr_printer[printer_devicelist_announce]: "
+    log_message(l_config, LOG_LEVEL_WARN, "rdpdr_printer[printer_devicelist_announce]: "
     		"Unable to add printer device");
     printer_device_list_reply(handle, STATUS_INVALID_PARAMETER);
   }
