b327e09b54ebd38d3515f023f3c6b184692ea300
samuel <samuel@c7ce0e6c-3862-4c07-88d1-63b9e066504f>
2010-11-16 16:58:43 +0000

    Improvement: better init script
    git-svn-id: file:///home/code/svn/xrdp/trunk@631 c7ce0e6c-3862-4c07-88d1-63b9e066504f

--- instfiles/xrdp.sh
+++ instfiles/xrdp.sh
@@ -15,47 +15,63 @@
 # chkconfig: 2345 11 89
 # description: starts xrdp
 
-#TODO: improve logs - start/stop daemon in right order
-
 SBINDIR=/usr/sbin
 CFGDIR=/etc/xrdp
 LOGDIR=/var/log/xrdp
 SPOOLDIR=/var/spool/xrdp
+PIDDIR=/var/run
 LOGDIR=/dev/null
 
 BINARIES="xrdp-logd xrdp-printerd xrdp xrdp-sesman"
 
 . /lib/lsb/init-functions
 
-for exe in $BINARIES; do
-    if ! [ -x "$SBINDIR/$exe" ]; then
-      log_warning_msg "$exe is not executable"
-      exit 0
+is_exec()
+{
+    if ! [ -x "$1" ]; then
+      log_warning_msg "$1 is not executable"
+      exit 1
     fi
-done
-if ! [ -x $CFGDIR/startwm.sh ]; then
-  log_warning_msg "startwm.sh is not executable"
-  exit 0
-fi
+}
 
 is_daemon_running()
 {
   DAEMON=$1
-  PID=/var/run/$DAEMON.pid
-  if [ -s $PID ] && kill -0 $(cat $PID) >/dev/null 2>&1; then
-    return 0;
+  PID=$PIDDIR/$DAEMON.pid
+  if [ -f $PID ]; then
+    if kill -0 $(cat $PID) 2>$LOGDIR; then
+      return 0;
+    else
+      rm $PID
+      return 1
+    fi
   else
     return 1;
   fi
 }
 
+for exe in $BINARIES; do
+    is_exec $SBINDIR/$exe
+done
+is_exec $CFGDIR/startwm.sh
+
 xrdp_start()
 {
   logoff all
-  [ -d $SPOOLDIR ] && mkdir -p $SPOOLDIR
+  FAILED=0
+  for exe in $BINARIES; do
+    if is_daemon_running $exe; then
+      FAILED=1
+      log_warning_msg "$exe is already loaded"
+    fi
+  done
+  if [ $FAILED -eq 1 ]; then
+    log_failure_msg "Do not try to launch another daemon !"
+    exit 1
+  fi
   for exe in $BINARIES; do
       log_daemon_msg "Starting $exe"
-      if start-stop-daemon --start --quiet --oknodo --pidfile /var/run/$exe.pid\
+      if start-stop-daemon --start --quiet --oknodo --pidfile $PIDDIR/$exe.pid \
                            --exec $SBINDIR/$exe -- >$LOGDIR
       then
         log_end_msg 0
@@ -63,13 +79,12 @@ xrdp_start()
         log_end_msg 1
       fi
   done
-  return 0;
 }
 
 xrdp_stop()
 {
   logoff all
-  for exe in $BINARIES; do
+  for exe in $(echo $BINARIES | tac -s ' '); do
       log_begin_msg "Stopping $exe"
       if ! is_daemon_running $exe; then
           log_progress_msg " (not loaded)"
@@ -77,50 +92,37 @@ xrdp_stop()
           continue
       fi
       if start-stop-daemon --stop --quiet --oknodo \
-                           --pidfile /var/run/$exe.pid --retry 30
+                           --pidfile $PIDDIR/$exe.pid --retry 30
       then
         log_end_msg 0
       else
         log_end_msg 1
       fi
   done;
-  return 0;
 }
 
 
 case "$1" in
 
   start)
-    FAILED=0
-    for exe in $BINARIES; do
-        if is_daemon_running $exe; then
-          FAILED=1
-          log_begin_msg "$exe is already loaded"
-          log_end_msg 1
-        fi
-    done
-    [ $FAILED -eq 1 ] && exit 1
     xrdp_start
-    ;;
+  ;;
 
   stop)
     xrdp_stop
-    ;;
+  ;;
 
   force-reload|restart)
-    echo "Restarting xrdp..."
+    log_success_msg "Restarting xrdp..."
     xrdp_stop
-    while ! is_daemon_running xrdp; do
-      sleep 1
-    done
     xrdp_start
-    ;;
+  ;;
 
   status)
     for exe in $BINARIES; do
-        status_of_proc -p "/var/run/$exe.pid" "$exe" $exe || exit $?
+        status_of_proc -p "$PIDDIR/$exe.pid" "$exe" $exe || exit $?
     done
-    ;;
+  ;;
 
   *)
     log_success_msg "Usage: /etc/init.d/xrdp {start|stop|restart|force-reload|status}"
